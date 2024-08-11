789b03690edbe846ffd58ebd6e5226080763ae3c
Lucas Máximo <lucasmaximo@outlook.com>
2024-08-10 21:28:31 -0300

    Implementação da lógica de adoição de contatos e redirecionamentos

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
